0d77cfd8131d27aba9dc2600d0eb6da34e092b91
Princess Diana B. Ongchangco <pdmb23@yahoo.com>
2015-03-13 17:18:56 +0800

    upload to github
    upload to github

---
+++ RomanNumeralsKata/RomanNumeralTest/RomanNumeralUnitTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeralsKata;
+
+namespace RomanNumeralTest
+{
+    [TestClass]
+    public class RomanNumeralUnitTest
+    {
+        [TestMethod]
+        public void ConvertDecimal1ToRomanNumeral_ShouldBe_I()
+        {
+            //ARRANGE
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //ACT
+            var actualResult = converter.Convert(1);
+            //ASSERT
+            var expectedResult = "I";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Decimal_3_Should_Should_Be_Roman_III()
+        {
+            //ARRANGE
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //ACT
+            var actualResult = converter.Convert(3);
+            //ASSERT
+            var expectedResult = "III";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Decimal_7_Should_Be_Roman_VII()
+        {
+            //ARRANGE
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //ACT
+            var actualResult = converter.Convert(7);
+            //ASSERT
+            var expectedResult = "VII";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Decimal_4_Should_Be_Roman_IV()
+        {
+            //ARRANGE
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //ACT
+            var actualResult = converter.Convert(4);
+            //ASSERT
+            var expectedResult = "IV";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Decimal_9_Should_Be_Roman_IX()
+        {
+            //ARRANGE
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            //ACT
+            var actualResult = converter.Convert(9);
+            //ASSERT
+            var expectedResult = "IX";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+        [TestMethod]
+        public void Decimal_14_Should_Be_Roman_XIV()
+        {
+            //ARRANGE
+            RomanNumeralConverter convert = new RomanNumeralConverter();
+            //ACT 
+            var actualResult = convert.Convert(14);
+            //ASSERT
+            var expectedResult = "XIV";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Decimal_1982_Should_Be_Roman_MCMLXXXII()
+        {
+            //ARRANGE
+            RomanNumeralConverter convert = new RomanNumeralConverter();
+            //ACT 
+            var actualResult = convert.Convert(1982);
+            //ASSERT
+            var expectedResult = "MCMLXXXII";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumeralConverter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumeralConverter
+    {
+        #region Fields
+        private Dictionary<int, string> _placeHolder;
+        private Dictionary<int, int> _pivot;
+        #endregion
+
+        #region Constructor
+        public RomanNumeralConverter()
+        {
+            _placeHolder = new Dictionary<int, string>() { 
+                {1, "I"}, 
+                {2, "V"},
+                {3, "X"},
+                {4, "L"}, 
+                {5, "C"}, 
+                {6, "D"}, 
+                {7, "M"}, 
+                {8, "Q"}
+            };
+            _pivot = new Dictionary<int, int>() {
+                {1, 2},
+                {2, 4},
+                {3, 6},
+                {4, 8}
+            };
+        }
+        #endregion
+
+        #region Methods
+        public string Convert(int number)
+        {
+            var retval = "";
+            var resultHolder = "";
+
+            var stringEquiv = number.ToString();
+            var stringEquivReverse = stringEquiv.Reverse().ToArray();
+
+            for (int count = 0; count < stringEquivReverse.Length; ++count)
+            {
+                var placeValue = int.Parse(stringEquivReverse[count].ToString());
+                number = placeValue;
+                var pivotIndex = count + 1;
+                while (number > 0)
+                {
+                    if (number >= 4)
+                    {
+                        if (number == 4 || number == 9)
+                        {
+                            retval = _placeHolder[_pivot[pivotIndex]-1];
+                        }
+                        if (number == 9)
+                        {
+                            retval += _placeHolder[_pivot[pivotIndex] + 1];
+                            break;
+                        }
+                        else
+                        {
+                            retval += _placeHolder[_pivot[pivotIndex]];
+                        }
+
+                        number = number - 5;
+                    }
+                    else if (number >= 1)
+                    {
+                        var one = _placeHolder[_pivot[pivotIndex] - 1];
+                        retval = retval + one;
+                        number -= 1;
+                    }
+                }
+                resultHolder = retval + resultHolder;
+                retval = "";
+            }
+
+            return resultHolder;
+        }
+        #endregion
+    }
+}
